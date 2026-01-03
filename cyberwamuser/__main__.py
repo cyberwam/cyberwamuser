c7e1270461892a89e7d77c7ae5c79bb6d893aef9
rickert155 <rickert1555@proton.me>
2026-01-03 21:07:01 +0400

    update __main__

--- cyberwamuser/__main__.py
+++ cyberwamuser/__main__.py
@@ -1,5 +1,11 @@
 from modules.core import init_tool 
 from modules.header import header
 
+def main():
+    # Создает в директории ~/.config свою директорию,
+    # перемещает в нее agent.json для генерирования 
+    # user-agent`ов для запросов
+    #init_tool()
+
 if __name__ == '__main__':
-    init_tool()
+    main()
